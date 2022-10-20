fb1227ee867cb020f63b90a29a23fb60aa850d23
immanuelk1m <98140860+immanuelk1m@users.noreply.github.com>
2022-10-20 18:57:42 +0900

    Update index_kospi.js

--- assets/js/index_kospi.js
+++ assets/js/index_kospi.js
@@ -30,7 +30,7 @@ loadItems().then((items) => {
         lineTension: 0,
         fill: false,
         borderColor: 'rgba(77,20,140)',
-        pointRadius: 0,
+        pointRadius: 1,
     };
 
     var dataSecond = {
@@ -40,7 +40,7 @@ loadItems().then((items) => {
         lineTension: 0,
         fill: false,
         borderColor: 'rgba(255,102,0)',
-        pointRadius: 0,
+        pointRadius: 1,
     };
 
     var speedData = {
