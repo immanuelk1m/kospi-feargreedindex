9382422e91ffccef0be818d926e597df34cc4e37
immanuelk1m <98140860+immanuelk1m@users.noreply.github.com>
2022-12-06 23:31:50 +0900

    Update vix_ema.js

--- assets/js/vix_ema.js
+++ assets/js/vix_ema.js
@@ -1,5 +1,5 @@
 function loadItems() {
-  return fetch("assets/js/json/vix_close.json") // change
+  return fetch("https://www.kospi-fear-greed-index.co.kr/assets/js/json/vix_close.json") // change
     .then((response) => response.json())
     .then((json) => json);
 }
