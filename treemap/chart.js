079532ac49cc01e6b356eaa4b7c64855d1ffdcab
immanuelk1m <98140860+immanuelk1m@users.noreply.github.com>
2022-12-11 23:27:16 +0900

    Update chart.js

--- treemap/chart.js
+++ treemap/chart.js
@@ -5,95 +5,92 @@ function loadItems() {
 }
 
 loadItems().then((items) => {
-  
-  var data = items.data.map(function(e) {
-      return e.series;
+  console().them(items);
   });
   
 
 
 
-  var options = {
-    data  
-    ,
-      chart: {
-      height: 350,
-      type: 'line',
-      zoom: {
-        enabled: false
-      },
-    },
-    dataLabels: {
+var options = {
+  data  
+  ,
+    chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
       enabled: false
     },
-    stroke: {
-      width: [5, 7, 5],
-      curve: 'straight',
-      dashArray: [0, 8, 5]
-    },
-    title: {
-      text: 'Page Statistics',
-      align: 'middle'
-    },
-    legend: {
-      tooltipHoverFormatter: function(val, opts) {
-        return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
-      }
-    },
-    markers: {
-      size: 0,
-      hover: {
-        sizeOffset: 6
-      }
-    },
-    xaxis: {
-      categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
-        '10 Jan', '11 Jan', '12 Jan'
-      ],
-      tickAmount: 2
-    },
-    yaxis : {
-      forceNiceScale: false,
-      max: 100,
-      labels: {
-        formatter: (value) => value.toFixed(0)+'%',
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    width: [5, 7, 5],
+    curve: 'straight',
+    dashArray: [0, 8, 5]
+  },
+  title: {
+    text: 'Page Statistics',
+    align: 'middle'
+  },
+  legend: {
+    tooltipHoverFormatter: function(val, opts) {
+      return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
     }
-    },
-      
+  },
+  markers: {
+    size: 0,
+    hover: {
+      sizeOffset: 6
+    }
+  },
+  xaxis: {
+    categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+      '10 Jan', '11 Jan', '12 Jan'
+    ],
+    tickAmount: 2
+  },
+  yaxis : {
+    forceNiceScale: false,
+    max: 100,
+    labels: {
+      formatter: (value) => value.toFixed(0)+'%',
+  }
+  },
     
-    tooltip: {
-      y: [
-        {
-          title: {
-            formatter: function (val) {
-              return val + " (mins)"
-            }
-          }
-        },
-        {
-          title: {
-            formatter: function (val) {
-              return val + " per session"
-            }
-          }
-        },
-        {
-          title: {
-            formatter: function (val) {
-              return val;
-            }
+  
+  tooltip: {
+    y: [
+      {
+        title: {
+          formatter: function (val) {
+            return val + " (mins)"
           }
         }
-      ]
-    },
-    grid: {
-      borderColor: '#f1f1f1',
-    }
-    };
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val + " per session"
+          }
+        }
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val;
+          }
+        }
+      }
+    ]
+  },
+  grid: {
+    borderColor: '#f1f1f1',
+  }
+  };
 
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
 
 
 
-});
