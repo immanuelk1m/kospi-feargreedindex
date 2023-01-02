24bedc1789e9bc77790581563b91f79e9c82d3a3
immanuelk1m <98140860+immanuelk1m@users.noreply.github.com>
2023-01-02 11:05:09 +0900

    Update safe_haven.js

--- assets/js/safe_haven.js
+++ assets/js/safe_haven.js
@@ -1,5 +1,5 @@
 function loadItems() {
-  return fetch("https://www.kospi-fear-greed-index.co.kr/assets/js/json/safe_spread.json") // change
+  return fetch("https://immanuelk1m.github.io/kospi-feargreedindex/assets/js/json/safe_spread.json") // change
     .then((response) => response.json())
     .then((json) => json);
 }
